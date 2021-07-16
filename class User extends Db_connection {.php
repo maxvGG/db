d5d8176bd87a2a7390bece67d974e475fac2d820
maxvGG <46627994+maxvGG@users.noreply.github.com>
2021-07-16 12:34:44 +0200

    Update class User extends Db_connection {.php

--- class User extends Db_connection {.php
+++ class User extends Db_connection {.php
@@ -10,23 +10,4 @@ class User extends Db_connection {
         $this->link = $db->connect();
         return $this->link;
     }
-
-
-
-    // create user function using pdo
-    // public function createUser($username, $email, $password) {
-    //     $sql = $this->link->prepare("INSERT INTO `user`(`username`, `email`, `password`) VALUES (:username, :email, :password)");
-    //     $sql->execute(array('username' => $username, 'email' => $email, 'password' => md5($password)));
-    //     // loop through $_POST variables to delete them after use 
-    //     // foreach ($_POST as $key => $value) {
-    //     //     $_POST[$key] = NULL;
-    //     // }
-    // }
-    // public function readall() {
-    //     $pdo = parent::$pdo;
-    //     $stmt = $pdo->prepare("SELECT id,username,email FROM `user` ORDER BY `user`.`id`  ASC");
-    //     $stmt->execute();
-    //     $results = $stmt->fetchAll();
-    //     return $results;
-    // }
 }
